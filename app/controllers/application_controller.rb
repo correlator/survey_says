041cd8402f50404697641192530b0fa169847b36
correlator <scotthoch86@gmail.com>
2017-05-16 14:55:25 -0700

    add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  http_basic_authenticate_with name: 'gemos', password: 'ConnectedData'
 
   def render_404
     head :not_found
